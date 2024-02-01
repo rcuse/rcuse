f5d8db52c9850d4cb96ea3b32bb7b5630e7d77b1
wangxingkang <wang_xingkang@qq.com>
2024-02-02 03:12:37 +0800

    chore(test): add alias config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,12 @@ import { resolve } from 'node:path'
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@rcuse/shared': resolve(__dirname, 'packages/shared/index.ts'),
+      '@rcuse/core': resolve(__dirname, 'packages/core/index.ts'),
+    },
+  },
   test: {
     environment: 'jsdom',
     setupFiles: [
