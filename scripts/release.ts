8eeed56fcde551db42a737c648f96606cd7d1a77
wangxingkang <wang_xingkang@qq.com>
2024-02-05 17:08:53 +0800

    chore: add publish scripts

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,5 +1,20 @@
-import { PATHS } from './.internal/constants';
+import { execSync } from 'node:child_process'
+import process from 'node:process'
+import { readJSONSync } from 'fs-extra'
 
-(async () => {
-  console.log(PATHS.ROOT);
-})();
+const { version: oldVersion } = readJSONSync('package.json')
+
+execSync('bumpp --no-commit --no-tag --no-push', { stdio: 'inherit' })
+
+const { version } = readJSONSync('package.json')
+
+if (oldVersion === version) {
+  console.log('canceled')
+  process.exit()
+}
+
+execSync('npm run update', { stdio: 'inherit' })
+execSync('git add .', { stdio: 'inherit' })
+
+execSync(`git commit -m "chore(release): v${version}"`, { stdio: 'inherit' })
+execSync(`git tag -a v${version} -m "v${version}"`, { stdio: 'inherit' })
