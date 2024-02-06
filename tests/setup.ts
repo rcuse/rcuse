c1a259eb7cfb1e40f928bb5ac54263bc3b3865b5
wangxingkang <wang_xingkang@qq.com>
2024-02-06 10:57:41 +0800

    chore(test): add polyfillPointerEvents

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,4 +1,12 @@
+import { beforeEach } from 'vitest'
+
 import '@testing-library/jest-dom/vitest'
 import './polyfillFetch'
+import './polyfillPointerEvents'
 import './polyfillIndexedDb'
 import './polyfillMatchMedia'
+
+beforeEach(() => {
+  document.body.innerHTML = ''
+  document.head.innerHTML = ''
+})
