cf967d9d02604636adbbfeaf064eacbb4b08d792
wangxingkang <wang_xingkang@qq.com>
2024-02-26 14:34:20 +0800

    chore: move dir

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,8 +11,7 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     setupFiles: [
-      resolve(__dirname, 'tests/jest-setup.ts'),
-      resolve(__dirname, 'tests/setup.ts')
+      resolve(__dirname, 'packages/.test/setup.ts')
     ],
     reporters: 'dot',
   }
