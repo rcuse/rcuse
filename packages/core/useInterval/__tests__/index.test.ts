1b823bdbaea75dde3f9e35589122000aff54662d
wangxingkang <wang_xingkang@qq.com>
2024-02-26 23:39:04 +0800

    feat: add useInterval

---
+++ packages/core/useInterval/__tests__/index.test.ts
@@ -0,0 +1,71 @@
+import { renderHook } from '@testing-library/react';
+import { beforeEach, afterEach, describe, expect, it, vi } from 'vitest';
+import { useInterval } from '../index';
+
+interface ParamsObj {
+  fn: (...arg: any) => any;
+  delay?: number;
+  options?: { immediate: boolean };
+}
+
+const setUp = ({ fn, delay, options }: ParamsObj) =>
+  renderHook(() => useInterval(fn, delay, options));
+
+describe('useInterval', () => {
+  beforeEach(() => {
+    vi.clearAllMocks()
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.clearAllTimers()
+  })
+
+  it('should be defined', () => {
+    expect(useInterval).toBeDefined();
+  });
+
+  it('interval should work', () => {
+    const callback = vi.fn();
+
+    setUp({ fn: callback, delay: 20, });
+    expect(callback).not.toBeCalled();
+
+    vi.advanceTimersByTime(70);
+    expect(callback).toHaveBeenCalledTimes(3);
+  });
+
+  it('delay is undefined should stop', () => {
+    const callback = vi.fn();
+
+    setUp({ fn: callback, delay: undefined });
+    vi.advanceTimersByTime(50);
+    expect(callback).toHaveBeenCalledTimes(0);
+
+    setUp({ fn: callback, delay: -2 });
+    vi.advanceTimersByTime(50);
+    expect(callback).toHaveBeenCalledTimes(0);
+  });
+
+  it('immediate in options should work', () => {
+    const callback = vi.fn();
+    setUp({ fn: callback, delay: 20, options: { immediate: true } });
+    expect(callback).toBeCalled();
+    expect(callback).toHaveBeenCalledTimes(1);
+    vi.advanceTimersByTime(50);
+    expect(callback).toHaveBeenCalledTimes(3);
+  });
+
+  it('interval should be clear', () => {
+    const callback = vi.fn();
+    const hook = setUp({ fn: callback, delay: 20 });
+
+    expect(callback).not.toBeCalled();
+
+    hook.result.current();
+    vi.advanceTimersByTime(70);
+    // not to be called
+    expect(callback).toHaveBeenCalledTimes(0);
+    // expect(clearInterval).toHaveBeenCalledTimes(1);
+  });
+})
---
+++ packages/core/useInterval/index.ts
@@ -0,0 +1,53 @@
+import { useCallback, useEffect, useRef } from 'react';
+import { useLatest } from '../useLatest';
+import { isNumber } from '@rcuse/shared'
+
+/**
+ * 处理 setInterval 的 Hook。
+ * @param fn 要定时调用的函数
+ * @param delay 定时时间（单位为毫秒），当取值 undefined 时会停止计时器
+ * @param options.immediate 是否在首次渲染时立即执行
+ */
+export function useInterval(
+  fn: () => void,
+  delay?: number,
+  options: {
+    immediate?: boolean;
+  } = {},
+) {
+  const { immediate } = options;
+
+  const fnRef = useLatest(fn);
+  const timerRef = useRef<ReturnType<typeof setInterval> | null>(null);
+
+  useEffect(
+    () => {
+      if (!isNumber(delay) || delay < 0) {
+        return;
+      }
+
+      if (immediate) {
+        fnRef.current();
+      }
+
+      timerRef.current = setInterval(() => {
+        fnRef.current();
+      }, delay);
+
+      return () => {
+        if (timerRef.current) {
+          clearInterval(timerRef.current);
+        }
+      };
+    },
+    [delay]
+  )
+
+  const clear = useCallback(() => {
+    if (timerRef.current) {
+      clearInterval(timerRef.current);
+    }
+  }, []);
+
+  return clear;
+}
