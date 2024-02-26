b5826b16bc18a205668921bd3e5b2165d46500ca
wangxingkang <wang_xingkang@qq.com>
2024-02-26 22:26:41 +0800

    feat(shared): add isString、isUndefined

--- packages/shared/utils/is.ts
+++ packages/shared/utils/is.ts
@@ -7,6 +7,35 @@ export const isClient = typeof window !== 'undefined' && typeof document !== 'un
 export const isNavigator = typeof navigator !== 'undefined';
 
 /**
+ * 检查 `value` 是否是一个字符串
+ *
+ * @param value 要检查的值
+ * @returns `value` 是字符串返回 `true`，否则返回 `false`
+ * @example
+ * ```ts
+ * isString('') // => true
+ * isString('hello') // => true
+ * ```
+ */
+export function isString(value: any): value is string {
+  return typeof value === 'string';
+}
+
+/**
+ * 检查 `value` 是否是 `undefined`
+ *
+ * @param value 要检查的值
+ * @returns `value` 是 `undefined` 返回 `true`，否则返回 `false`
+ * @example
+ * ```ts
+ * isUndefined(undefined) // => true
+ * ```
+ */
+export function isUndefined(value: any): value is undefined {
+  return value === undefined;
+}
+
+/**
  * 判断值是否为一个方法
  * @param val
  * @returns
