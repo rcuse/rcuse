547eb12b23a904b14754937f4f2ede4b4ee453b9
wangxingkang <wang_xingkang@qq.com>
2024-02-26 23:08:26 +0800

    feat(shared): add isNumber

--- packages/shared/utils/is.ts
+++ packages/shared/utils/is.ts
@@ -7,6 +7,23 @@ export const isClient = typeof window !== 'undefined' && typeof document !== 'un
 export const isNavigator = typeof navigator !== 'undefined';
 
 /**
+ * 检查 `value` 是否是一个数字。
+ *
+ * @param value 要检查的值
+ * @returns `value` 是数字返回 `true`，否则返回 `false`
+ * @example
+ * ```ts
+ * isNumber(1) // => true
+ * isNumber(0.1) // => true
+ * isNumber(NaN) // => false
+ * isNumber(Infinity) // => false
+ * ```
+ */
+export function isNumber(value: any): value is number {
+  return typeof value === 'number';
+}
+
+/**
  * 检查 `value` 是否是一个字符串
  *
  * @param value 要检查的值
