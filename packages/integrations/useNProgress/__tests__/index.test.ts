5649adbb33a408e0ba51f22da4aad04cff1b36d9
wangxingkang <wang_xingkang@qq.com>
2024-03-12 16:13:40 +0800

    test(useNProgress): add test

---
+++ packages/integrations/useNProgress/__tests__/index.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from 'vitest'
+import { useNProgress } from '../index'
+
+describe('useNProgress', () => {
+  it('should be defined', () => {
+    expect(useNProgress).toBeDefined()
+  })
+})
