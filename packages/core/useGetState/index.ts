706bb986cec30a85d6fe51f772201a6b40a8cd46
wangxingkang <wang_xingkang@qq.com>
2024-03-14 11:26:22 +0800

    feat(core): 代码优化

--- packages/core/useGetState/index.ts
+++ packages/core/useGetState/index.ts
@@ -1,23 +1,24 @@
-import React, { useState, useRef, useCallback } from 'react';
+import type React from 'react'
+import { useCallback, useState } from 'react'
+import { useLatest } from '../useLatest'
 
-type GetStateAction<S> = () => S;
+type GetStateAction<S> = () => S
 
 function useGetState<S>(
   initialState: S | (() => S),
-): [S, React.Dispatch<React.SetStateAction<S>>, GetStateAction<S>];
+): [S, React.Dispatch<React.SetStateAction<S>>, GetStateAction<S>]
 function useGetState<S = undefined>(): [
   S | undefined,
   React.Dispatch<React.SetStateAction<S | undefined>>,
   GetStateAction<S | undefined>,
-];
+]
 function useGetState<S>(initialState?: S) {
-  const [state, setState] = useState(initialState);
-  const stateRef = useRef(state);
-  stateRef.current = state;
+  const [state, setState] = useState(initialState)
+  const stateRef = useLatest(state)
 
-  const getState = useCallback(() => stateRef.current, []);
+  const getState = useCallback(() => stateRef.current, [])
 
-  return [state, setState, getState];
+  return [state, setState, getState]
 }
 
 export {
