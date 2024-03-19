38d684c3f735659638c1e378d95c72ff81f715ed
wangxingkang <wang_xingkang@qq.com>
2024-03-19 22:51:16 +0800

    fix(shared): 修复引用报错

--- packages/shared/domTarget/index.ts
+++ packages/shared/domTarget/index.ts
@@ -1,32 +1,30 @@
-import React from 'react';
-import { isClient, isFunction } from '../utils';
+import type React from 'react'
+import { isFunction } from 'radash'
+import { isClient } from '../utils'
 
-type TargetValue<T> = T | undefined | null;
-type TargetType = HTMLElement | Element | Window | Document;
+type TargetValue<T> = T | undefined | null
+type TargetType = HTMLElement | Element | Window | Document
 
 export type BasicTarget<T extends TargetType = Element> =
   | (() => TargetValue<T>)
   | TargetValue<T>
-  | React.MutableRefObject<TargetValue<T>>;
+  | React.MutableRefObject<TargetValue<T>>
 
 export function getTargetElement<T extends TargetType>(target: BasicTarget<T>, defaultElement?: T) {
-  if (!isClient) {
-    return undefined;
-  }
+  if (!isClient)
+    return undefined
 
-  if (!target) {
-    return defaultElement;
-  }
+  if (!target)
+    return defaultElement
 
-  let targetElement: TargetValue<T>;
+  let targetElement: TargetValue<T>
 
-  if (isFunction(target)) {
-    targetElement = target();
-  } else if ('current' in target) {
-    targetElement = target.current;
-  } else {
-    targetElement = target;
-  }
+  if (isFunction(target))
+    targetElement = target()
+  else if ('current' in target)
+    targetElement = target.current
+  else
+    targetElement = target
 
-  return targetElement;
+  return targetElement
 }
