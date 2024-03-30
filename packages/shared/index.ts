6c028252bff198176edb5054e45450ee2e12668f
wangxingkang <wang_xingkang@qq.com>
2024-03-31 00:13:33 +0800

    feat(shared): replace isEqual

--- packages/shared/index.ts
+++ packages/shared/index.ts
@@ -1,6 +1,114 @@
 export * from './domTarget'
+export * from './isEqual'
 export * from './utils'
 export * from './uuid'
 export * from './dedent'
 export * from './indent'
-export * from 'radash'
+export {
+  // Array
+  alphabetical,
+  boil,
+  cluster,
+  counting,
+  diff,
+  first,
+  flat,
+  fork,
+  group,
+  intersects,
+  iterate,
+  last,
+  list,
+  max,
+  merge,
+
+  // Async
+  all,
+  defer,
+  guard,
+  map,
+  parallel,
+  reduce,
+  retry,
+  sleep,
+  tryit,
+  min,
+  objectify,
+  range,
+  replaceOrAppend,
+  replace,
+  select,
+  shift,
+  sift,
+  sort,
+  sum,
+  toggle,
+  unique,
+  zipToObject,
+  zip,
+
+  // Curry
+  chain,
+  compose,
+  debounce,
+  memo,
+  partial,
+  partob,
+  proxied,
+  throttle,
+
+  // Number
+  inRange,
+  toFloat,
+  toInt,
+
+  // Object
+  assign,
+  clone,
+  construct,
+  crush,
+  get,
+  invert,
+  keys,
+  listify,
+  lowerize,
+  mapEntries,
+  mapKeys,
+  mapValues,
+  omit,
+  pick,
+  set,
+  shake,
+  upperize,
+
+  // Random
+  draw,
+  random,
+  shuffle,
+  uid,
+  series,
+
+  // String
+  camel,
+  capitalize,
+  dash,
+  pascal,
+  snake,
+  template,
+  title,
+  trim,
+
+  // Typed
+  isArray,
+  isNumber,
+  isObject,
+  isDate,
+  isFloat,
+  isFunction,
+  isEmpty,
+  isPromise,
+  isInt,
+  isPrimitive,
+  isSymbol,
+  isString,
+} from 'radash'
