68d5ea5b1bbbf6c37e6c62d1ad65e64a971d20ba
wangxingkang <wang_xingkang@qq.com>
2024-06-26 11:15:25 +0800

    feat: add useIntersectionObserver

---
+++ packages/core/useIntersectionObserver/__tests__/index.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from 'vitest'
+import { useIntersectionObserver } from '../index'
+
+describe('useIntersectionObserver', () => {
+  it('should be defined', () => {
+    expect(useIntersectionObserver).toBeDefined()
+  })
+})
