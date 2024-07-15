442dbfa27b7aa4ee24a1b7c8b56c223a5ecee28e
wangxingkang <wang_xingkang@qq.com>
2024-07-15 22:47:23 +0800

    chore: format code

--- packages/shared/uuid/index.ts
+++ packages/shared/uuid/index.ts
@@ -1,32 +1,32 @@
 /**
  * @description Return unique UUID v4
  */
-export const uuid = () => {
-  let uuid = '';
+export function uuid() {
+  let uuid = ''
 
   for (let i = 0; i < 32; i++) {
     switch (i) {
       case 8:
       case 20: {
-        uuid += `-${Math.trunc(Math.random() * 16).toString(16)}`;
+        uuid += `-${Math.trunc(Math.random() * 16).toString(16)}`
 
-        break;
+        break
       }
       case 12: {
-        uuid += '-4';
+        uuid += '-4'
 
-        break;
+        break
       }
       case 16: {
-        uuid += `-${((Math.random() * 16) | (0 & 3) | 8).toString(16)}`;
+        uuid += `-${((Math.random() * 16) | (0 & 3) | 8).toString(16)}`
 
-        break;
+        break
       }
       default: {
-        uuid += Math.trunc(Math.random() * 16).toString(16);
+        uuid += Math.trunc(Math.random() * 16).toString(16)
       }
     }
   }
 
-  return uuid;
-};
+  return uuid
+}
